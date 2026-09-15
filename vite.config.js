6a5c02663400ca4953313ceeb7ad5cbf9f283d38
chromaly <ryancy18@gmail.com>
2026-09-14 18:21:07 -0700

    allowing capacitor ios

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/planner.io/'
+  base: process.env.CAPACITOR_BUILD ? '/' : '/planner.io/'
 })
